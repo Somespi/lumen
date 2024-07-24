f715dcbf95bd041049e36285e5b1541001a64a3b
Someone. <120342697+Somespi@users.noreply.github.com>
2024-07-24 22:52:41 +0400

    add bytecodes

---
+++ src/structures/bytecodes.php
@@ -0,0 +1,15 @@
+<?php 
+enum Bytecode : int {
+    case Push = 0;
+    case Pop = 1;
+    case Move = 2;
+    case Dup = 3;
+    case Add = 4;
+    case Sub = 5;
+    case Mult = 6;
+    case Jump = 7;
+    case JumpIf = 8;
+    case JumpIfNot = 9;
+    case NOP = 10;
+
+}
